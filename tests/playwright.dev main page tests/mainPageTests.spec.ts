c9bcdfb1c00174f7c361ccc49158508419abfbab
Maxdeoro <maxdeoro@gmail.com>
2026-03-05 21:28:00 +0300

    annotations in tests

--- tests/playwright.dev main page tests/mainPageTests.spec.ts
+++ tests/playwright.dev main page tests/mainPageTests.spec.ts
@@ -6,20 +6,20 @@ test.describe('tests main page', () => {
     await page.goto('https://playwright.dev');
   });
 
-  test('test button Get started', async ({ page }) => {
+  test.skip('test button Get started', async ({ page }) => {
     await expect.soft(page.getByRole('link', { name: 'Get started' })).toBeVisible();
     await expect.soft(page.getByRole('link', {name: 'Get started'})).toContainText('Get started');
     await expect.soft(page.getByRole('link', { name: 'Get started' })).toHaveAttribute('href','/docs/intro');
   });
 
-  test('test href attributs', async ({ page }) => {
+  test.fixme('test href attributs', async ({ page }) => {
     await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toHaveAttribute('href','/');
     await expect(page.getByRole('link', { name: 'Docs' })).toHaveAttribute('href','/docs/intro');
     await expect(page.getByRole('link', { name: 'API' })).toHaveAttribute('href','/docs/api/class-playwright');
     await expect(page.getByRole('link', { name: 'Community' })).toHaveAttribute('href','/community/welcome');
   });
 
-  test('test page heading', async ({ page }) => {
+  test.only('test page heading', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toContainText('Playwright enables reliable end-to-end testing for modern web apps.');
   });
